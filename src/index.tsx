bf56962f0415637384f5e94fb5466d7e0c338e50
jarinykanen <jarinykanen.koodausta@gmail.com>
2021-09-22 14:09:02 +0300

    Removed unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { CssBaseline, responsiveFontSizes, ThemeProvider } from "@material-ui/core";
-import { store } from "./app/store";
+import { store } from "app/store";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-import theme from "./theme/theme";
+import App from "components/App";
+import reportWebVitals from "reportWebVitals";
+import theme from "theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
